66e06eed72b704c6dc1a7ccf423d9720d4a1df6c
Leonardo-F-Santana <discipuloleonardo@gmail.com>
2024-06-20 19:17:09 -0300

    aula14 e 15

---
+++ aula14.py
@@ -0,0 +1,18 @@
+#Formatação de strings
+# Quando uma função está dentro de um objeto, é chamada de método
+a = 'AAA'
+b = 'BBB'
+c = 1.1
+string = 'a={0} b={1} c={2:.2f}'
+formato = string.format(a, b, c)
+print(formato)
+
+
+a = 'AAA'
+b = 'BBB'
+c = 1.1
+string = 'a={nome1} b={nome2} c={nome3:.2f}'
+formato = string.format(
+    nome1=a, nome2=b, nome3=c)
+
+print(formato) 
