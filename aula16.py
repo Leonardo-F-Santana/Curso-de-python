6686586dddb5104117e67efdc7465555c5ad81c4
Leonardo-F-Santana <discipuloleonardo@gmail.com>
2024-06-25 21:11:52 -0300

    If / Else

--- aula16.py
+++ aula16.py
@@ -5,3 +5,9 @@ entrada = input('Voce quer entrar ou sair? ')
 
 if entrada == 'entrar':
     print('Você entrou no sistema')
+
+elif entrada == 'sair':
+    print('Você saiu do sistema')
+
+else:
+    print('Você não digitou nem entrar e nem sair')
