cb0fd8447b1849fa32072b4dcd106e3ba39828e4
Leonardo-F-Santana <discipuloleonardo@gmail.com>
2024-06-25 21:31:12 -0300

    Cadastro de cliente simples

---
+++ Cadastro_cliente.py
@@ -0,0 +1,6 @@
+Id = input('Digite o Id do cliente: ')
+nome = input('Digite o nome do cliente: ')
+cpf = input('Digite o CPF do cliente: ')
+renda = input('Digite a renda: ')
+
+print(f'Cliente {Id}, com nome {nome}, de CPF {cpf}, tem renda de: {renda}')
