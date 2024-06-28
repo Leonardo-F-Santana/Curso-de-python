8b813df60222506ee08e55f5eaf156c4417e0b64
Leonardo-F-Santana <discipuloleonardo@gmail.com>
2024-06-28 10:50:55 -0300

    Operadores_Relacionais

---
+++ aula19.py
@@ -0,0 +1,22 @@
+''' 
+Operadores de comparação (relacionais)
+OP = Significado - Exemplo (True)
+> maior = 2 > 1
+>= Maior ou igual 2 >= 2
+< menor = 1 < 2
+<= Menor ou igual 2 <= 2
+== Igual 'a' == 'a'
+!= Diferente 'a' != 'b'
+'''
+maior = 2 > 1
+maior_ou_igual = 2 >= 2
+menor = 1 < 0
+menor_ou_igual = 2 <= 2
+igual = 'a' == 'l'
+diferente = 'a' != 'b'
+print(maior)
+print(maior_ou_igual)
+print(menor)
+print(menor_ou_igual)
+print(igual)
+print(diferente)
