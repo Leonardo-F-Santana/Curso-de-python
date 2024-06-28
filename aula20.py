771eab614f014280e7405ff3688d10825446ac06
Leonardo-F-Santana <discipuloleonardo@gmail.com>
2024-06-28 11:00:43 -0300

    Exercicio if / else

---
+++ aula20.py
@@ -0,0 +1,7 @@
+primeiro_valor = input('Primeiro valor: ')
+segundo_valor = input('Segundo valor: ')
+
+if primeiro_valor > segundo_valor:
+    print(f'Primeiro valor {primeiro_valor} maior que o Segundo valor {segundo_valor}')
+else:
+    print(f'Segundo valor {segundo_valor} maior que o Primeiro valor {primeiro_valor}')
