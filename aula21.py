2da453e3b7c2c68ded0fd85a0a4175fe5167211a
Leonardo-F-Santana <discipuloleonardo@gmail.com>
2024-07-04 15:33:26 -0300

    Operadores lógicos AND - OR - NOT

---
+++ aula21.py
@@ -0,0 +1,16 @@
+# Operadores lógicos
+# and - or - not
+# and - todas as condições precisam ser verdadeiras
+# se qualquer valor for falso,
+# a expressão inteira será avaliada naquele ValueErrorSão considerados False
+# 0.0.0 '' FalseTambém existe o tipo None que é
+# usado para representar um não valor
+
+entrada = input('[E]ntrar [S]air:')
+senha_digitada = input('Senha: ')
+
+senha_permitida = '123456'
+if entrada == 'E'and senha_digitada == senha_permitida:
+    print('Entrar')
+else:
+    print('Sair')
