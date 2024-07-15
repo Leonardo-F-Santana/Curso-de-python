5ea4284c299f36d34d037cbb13b5a31c87a283e6
Leonardo-F-Santana <discipuloleonardo@gmail.com>
2024-07-15 12:51:08 -0300

    Try / except

--- aula29.py
+++ aula29.py
@@ -4,4 +4,13 @@ try -> tentar executar o código
 except -> ocorreu algum erro ao tentar executar
 """
 
-numero_str = input('Vou dobrar o numero que voce digitar')
+numero_str = input('Vou dobrar o numero que voce digitar: ')
+try:
+    print('str: ', numero_str)
+    numero_float = float(numero_str) 
+    print('float: ', numero_float)
+    print(f'O dobro de {numero_str} é: {numero_float * 2}')
+except:
+    print('Isso não é um número')
+
+
