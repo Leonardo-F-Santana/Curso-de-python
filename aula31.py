93899bee9958234859d487e7ebdb4644bc5e66d5
Leonardo-F-Santana <discipuloleonardo@gmail.com>
2024-07-16 19:59:29 -0300

    Id = Identidade das variaveis

---
+++ aula31.py
@@ -0,0 +1,14 @@
+"""
+Flag (bandeira) - Marcar um local
+None = não valor
+is e is not = e ou não é ( tipo, valor, idadentidade)
+id = Identidade
+"""
+v1 = 'a'
+v2 = 'b'
+print(id(v1))
+print(id(v2))
+
+# condição = false
+# if comdição:
+#   print('Faça algo')
