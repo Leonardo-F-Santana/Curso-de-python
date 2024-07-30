db02ae0aef39ac34530042870c26986b9dfbf074
Leonardo-F-Santana <discipuloleonardo@gmail.com>
2024-07-30 13:20:09 -0300

    aula32

--- aula32.py
+++ aula32.py
@@ -3,16 +3,16 @@ Faça um programa que peça ao usuario um numro inteiro,
 informe se este número é par ou ímpar. Caso o usuario não digite um número
 inteiro, informe que não é um número inteiro.
 '''
-numero = input('Digite um número: ')
+# numero = input('Digite um número: ')
 
-try:
-    numero = int(numero)
-    if numero % 2 == 0:
-        print(f'O número {numero} é par!')
-    else:
-        print(f'O número {numero} é ímpar!')
-except:
-    print('Você não digitou um número inteiro!')
+# try:
+#     numero = int(numero)
+#     if numero % 2 == 0:
+#         print(f'O número {numero} é par!')
+#     else:
+#         print(f'O número {numero} é ímpar!')
+# except:
+#     print('Você não digitou um número inteiro!')
 
 
 
@@ -24,8 +24,11 @@ Bom dia 0-11, boa tarde 12-17, boa noite 18-23
 # hora = input('Digite a hora: ')
 # if >=0 and <=11:
 #     print('Bom dia')
-
-
+hora = input('Digite a hora')
+try:
+    hora = int(hora)
+    if hora == 0 :
+        print('Bom dia!')
 
 
 
