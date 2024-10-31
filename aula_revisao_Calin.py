10f39b40931cf785ca52b0ac14fd79050992799d
Leonardo-F-Santana <discipuloleonardo@gmail.com>
2024-10-31 10:20:59 -0300

    aula_revisao_Calin

--- aula_revisao_Calin.py
+++ aula_revisao_Calin.py
@@ -9,6 +9,28 @@
 #     print(f"fValor da prestação: R$ {prestacao:.2f} Empréstimo OK" )
 
 
+# Faça um programa que solicite ao usuário seu
+# nome, sua idade, seu peso e sua altura.
+# O programa deve:
+# Calcular o IMC pela formula IMC=
+# peso/(altura*altura)
+# Exibir uma mensagem com o nome do
+# usuário, a idade e o valor do IMC.
+# OBS: Atenção as variáveis que devem ser
+# inteiras e as que devem ser float.
+# Exibir o IMC com duas casas decimais.
+# nome = input("Digite seu nome: ")
+# idade = int(input('Digite sua idade: '))
+# peso = float(input("Digite seu peso: "))
+# altura = float(input("Digite sua altura: "))
+# IMC = peso/(altura*altura)
+
+# print(f"O paciente {nome} com idade de {idade}, está com {peso}kg e possui IMC de {IMC}")
+
+
+
+
+
 # Fazer um programa que leia duas notas de alunos, calcular a média desses alunos e imprimir quantos são aprovados (média maior ou igual a 7,0),
 # quantos estão em exame final (média entre 4,0 e 7,0) ou quantos estão reprovados (média menor que 4,0). O programa deve solicitar quantos alunos possui a turma
 # e imprimir um quantitativo em cada um dos três casos. EXIBIR O CÓDIGO COM O PRINT RODANDO
@@ -27,9 +49,9 @@
 
 
 
-contador = 0
-while contador < 10:
-    contador = contador + 1
-    print(contador)
-print('Fim')
+# contador = 0
+# while contador < 10:
+#     contador = contador + 1
+#     print(contador)
+# print('Fim')
 
